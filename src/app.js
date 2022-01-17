88e5f0b1fa21a0cb8a7f8f8ad20ea7b90883a6b2
chamath silva <chamath.silva@syscolabs.com>
2022-01-17 16:41:12 +0530

    test : pipeline

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ initializeConfigs().then(() => {
       nodeEnv: process.env.NODE_ENV,
       deploymentEnv: process.env.STAGE,
       versionId: process.env.VERSION_ID,
-      version: 'blue-12',
+      version: 'blue-13',
       envs: process.env
     };
     res.status(HttpStatus.OK).send(createSuccessResponse(health));
